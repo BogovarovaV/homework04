5521680dd797a853a26528e5d1131b4f99d13d87
Валентина <valentinka-2892@yandex.ru>
2021-12-08 18:24:53 +0500

    Homework 2.5

--- src/main/java/pro/sky/java/course2/homework04/EmployeeController.java
+++ src/main/java/pro/sky/java/course2/homework04/EmployeeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
 @RequestMapping("/employee")
 
@@ -30,5 +32,11 @@ public class EmployeeController {
     @GetMapping("/find")
     public Employee findEmployee(@RequestParam String firstName, @RequestParam String lastName) {
         return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/print")
+    public Set<Employee> printEmployees(Set<Employee> employees) {
+        employeeService.printEmployees(employees);
+        return (employees);
     }
 }
